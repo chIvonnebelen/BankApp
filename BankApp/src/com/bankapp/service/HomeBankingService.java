a382e4b8e27be1d1e32da1603e9163e92f90347e
Ivonne <ivonebelenona@gmail.com>
2022-12-02 12:02:53 -0300

    Login

--- BankApp/src/com/bankapp/service/HomeBankingService.java
+++ BankApp/src/com/bankapp/service/HomeBankingService.java
@@ -1,4 +1,40 @@
 package com.bankapp.service;
 
+import com.bankapp.entity.HomeBanking;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class HomeBankingService {
+    private final Scanner sc = new Scanner(System.in);
+
+    public HomeBanking login(List<HomeBanking> homeBankings) {
+        HomeBanking account = null; //inicializarlo
+
+        System.out.println("Enter the username :");
+        String usersame = sc.next();
+
+        for (HomeBanking homeBanking : homeBankings) {
+            if (homeBanking.getUsername().equals(usersame)){
+                account = homeBanking;
+            }
+        }
+        if (account != null){
+            System.out.println("Enter password");
+            String password = sc.next();
+            if (account.getPassword().equals(password)){
+                System.out.println("You have entered correctly.. ");
+            } else {
+                System.out.println("Wrong password");
+            }
+        }else {
+            System.out.println("Invalid user");
+        }
+
+        return account;
+    }
+
 }
+
+
+
